3611f1d6b7f51f46b976e90463eb52641abb4f52
gytdau <gytdau@gmail.com>
2016-03-12 17:54:41 +0000

    :new: Added new emojis

--- emoji-list.js
+++ emoji-list.js
@@ -27,12 +27,60 @@ var emojis = {
         {
             css: 'emj1172',
             code: ':orange_book:'
+        },
+        {
+            css: 'emj1176',
+            code: ':pencil:'
         }
     ],
-    fixes: [
+    bug: [
         {
             css: 'emj1159',
             code: ':pushpin:'
+        },
+        {
+            css: 'emj184',
+            code: ':heavy_exclamation_mark:'
+        },
+        {
+            css: 'emj15',
+            code: ':bangbang:'
+        },
+        {
+            css: 'emj179',
+            code: ':x:'
+        }
+    ],
+    celebration: [
+        {
+            css: 'emj626',
+            code: ':confetti_ball:'
+        },
+        {
+            css: 'emj625',
+            code: ':tada:'
+        }
+    ],
+    newFeature: [
+        {
+            css: 'emj59',
+            code: ':ballot_box_with_check:'
+        },
+        {
+            css: 'emj174',
+            code: ':sparkles:'
+        },
+        {
+            css: 'emj217',
+            code: ':new:'
+        },
+        {
+            css: 'emj1115',
+            code: ':boom:'
+        },
+        {
+            css: 'emj516',
+            code: ':star2:'
         }
     ]
 };
